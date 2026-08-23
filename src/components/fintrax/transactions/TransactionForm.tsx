d27088fe8f692e94c95acf94cde0c31419089ab8
Hakeem Polistico <hjpolistico@gmail.com>
2026-08-23 16:26:17 +0800

    fix: allow clearing account on transaction edit

--- src/components/fintrax/transactions/TransactionForm.tsx
+++ src/components/fintrax/transactions/TransactionForm.tsx
@@ -166,6 +166,8 @@ export default function TransactionForm({
               defaultValue={relationshipId(data.account as any) ?? ''}
               options={accountOptions}
               placeholder="Select account used"
+              allowEmpty={mode === 'edit'}
+              emptyLabel="No account"
               onChange={(value) => setData({ ...data, account: value || null })}
             />
             <p className="mt-1 text-xs text-gray-500 dark:text-gray-400">
@@ -268,6 +270,8 @@ export default function TransactionForm({
               defaultValue={relationshipId(data.account as any) ?? ''}
               options={accountOptions}
               placeholder="Select account"
+              allowEmpty={mode === 'edit'}
+              emptyLabel="No account"
               onChange={(value) => setData({ ...data, account: value || null })}
             />
           </div>
